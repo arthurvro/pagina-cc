8a29534f657136b75ba167d6834e4f1e03075d22
Arthur VR <arthurolov@gmail.com>
2024-04-08 17:27:04 -0300

    feat: atualização dos Headers Components

--- src/components/Header/HeaderBE.js
+++ src/components/Header/HeaderBE.js
@@ -4,9 +4,13 @@ import BG from '../Assets/logo-shadow.png';
 export const HeaderBE = () => {
 
     return (
-        <div className='div-central'>
-            <img src={BG} alt='BG-SHADOW-LOGO'/>
-            <span>test</span>
+        <div className='container'>
+            <div className='head-content'>
+                <h1>TESTE</h1>
+                <img src={BG} alt='BG-SHADOW-LOGO'/>
+            </div>
+           
         </div>
+        
     );
 }
